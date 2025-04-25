528efc08ea3396b89de8103c8452e73e0a0dc84e
HalaKorhani <halakorhani2005@icloud.com>
2025-04-25 22:37:18 +0300

    Insert new user into database using prepared statement

--- DataAuth/register.php
+++ DataAuth/register.php
@@ -17,4 +17,15 @@ $email = trim($_POST["email"]);
 $pass = trim($_POST["password"]);
 $gender = trim($_POST["gender"]);
 $country = trim($_POST["country"]);
+$sql = "INSERT INTO users (fname, lname, address, email, password, genderID, countryID)
+        VALUES (:fname, :lname, :address, :email, :password, :gender, :country);";
+$stmt = $pdo -> prepare($sql);
+$stmt -> bindParam(":fname", $fname );
+$stmt -> bindParam(":lname", $lname );
+$stmt -> bindParam(":address", $address);
+$stmt -> bindParam(":email",  $email );
+$stmt -> bindParam(":password",$pass);   
+$stmt -> bindParam(":gender", $gender );
+$stmt -> bindParam(":country", $country );
+$stmt ->execute();
 
