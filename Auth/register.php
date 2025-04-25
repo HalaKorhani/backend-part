702ab773dce39f8d31086c0b9060c8a224b875cf
HalaKorhani <halakorhani2005@icloud.com>
2025-04-25 20:29:52 +0300

    Initial registration form layout

--- Auth/register.php
+++ Auth/register.php
@@ -1,21 +1,15 @@
-<div class="mb-3">
-    <label for="firstName" class="form-label">First Name</label>
-    <input name="fname" type="text" class="form-control" id="firstName" required>
-</div>
-<div class="mb-3">
-    <label for="lastName" class="form-label">Last Name</label>
-    <input name="lname" type="text" class="form-control" id="lastName" required>
-</div>
-<div class="mb-3">
-    <label for="address" class="form-label">Address</label>
-    <input name="address" type="text" class="form-control" id="address" required>
-</div>
-<div class="mb-3">
-    <label for="email" class="form-label">Email</label>
-    <input name="email" type="email" class="form-control" id="email" required>
-</div>
-<div class="mb-3">
-    <label for="password" class="form-label">Password</label>
-    <input name="password" type="password" class="form-control" id="password" required>
-</div>
-<title>Registration</title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="../css/register.css">
+    <title>Registration</title>
+    <title>register</title>
+</head>
+<body>
+    <h1 class="register">Registration</h1>
+    <div class="container">
+        <form action="../DataAuth/register.php" method="POST">
