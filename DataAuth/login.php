73ffcca1cbaeb88c09da5208a4aa90cd868c5d9b
HalaKorhani <halakorhani2005@icloud.com>
2025-04-25 22:31:21 +0300

    Set login session and redirect on successful password match

--- DataAuth/login.php
+++ DataAuth/login.php
@@ -11,10 +11,17 @@ if (isset($_POST["email"]) && isset($_POST["password"]) &&
         $stmt = $pdo->prepare($sql);
         $stmt->bindParam(":email", $email);
         $stmt->execute();
-        $user1 = $stmt->fetch(PDO::FETCH_ASSOC)
+        $user1 = $stmt->fetch(PDO::FETCH_ASSOC);
         if (!$user1) {
             $_SESSION["login"] = false;
             $_SESSION["msg"] = "incorrect username or password";
             header("location:../Auth/login.php");
             die();
         }        
+
+    } elseif ($pass === $user1["password"]) { 
+        $_SESSION["login"] = true;
+        header("location:../index.php");
+        die();
+    } 
+    
