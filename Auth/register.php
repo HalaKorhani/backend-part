efac5cd523fe66d751bcefa7a0b8fa3951cb13cd
HalaKorhani <halakorhani2005@icloud.com>
2025-04-25 20:30:34 +0300

    "Add form input fields with placeholder IDs (to be fixed)"

--- Auth/register.php
+++ Auth/register.php
@@ -13,3 +13,23 @@
     <h1 class="register">Registration</h1>
     <div class="container">
         <form action="../DataAuth/register.php" method="POST">
+        <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">First Name</label>
+    <input name="fname" type="text" class="form-control" id="exampleInputEmail1" required>
+</div>
+<div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Last Name</label>
+    <input name="lname" type="text" class="form-control" id="exampleInputEmail1" required>
+</div>
+<div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Address</label>
+    <input name="address" type="text" class="form-control" id="exampleInputEmail1" required>
+</div>
+<div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email</label>
+    <input name="email" type="email" class="form-control" id="exampleInputEmail1" required>
+</div>
+<div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Password</label>
+    <input name="password" type="text" class="form-control" id="exampleInputEmail1" required>
+</div>
