6d7ac708dbfcf46635ca9d2c3716e1ca8bb03e4a
HalaKorhani <halakorhani2005@icloud.com>
2025-04-25 22:29:46 +0300

    "Sanitize input and include database connection

--- DataAuth/login.php
+++ DataAuth/login.php
@@ -3,3 +3,8 @@ session_start();
 ob_start(); 
 if (isset($_POST["email"]) && isset($_POST["password"]) &&
     !empty(trim($_POST["email"])) && !empty(trim($_POST["password"]))) {
+        $email = trim($_POST["email"]);
+        $pass = trim($_POST["password"]);
+        
+        require_once("../connection.php");
+        
