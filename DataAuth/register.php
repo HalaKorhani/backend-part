b4456ea75a90f779d0943035d07f1789d011165d
HalaKorhani <halakorhani2005@icloud.com>
2025-04-25 22:35:34 +0300

    Validate presence of all registration fields

--- DataAuth/register.php
+++ DataAuth/register.php
@@ -1,2 +1,11 @@
 <?php 
 session_start();
+if( 
+    isset($_POST["fname"]) && !empty(trim($_POST["fname"]))  &&
+    isset($_POST["lname"]) && !empty(trim($_POST["lname"])) &&
+    isset($_POST["address"]) && !empty(trim($_POST["address"])) &&
+    isset($_POST["email"]) && !empty(trim($_POST["email"])) &&
+    isset($_POST["password"]) && !empty(trim($_POST["password"])) &&
+    isset($_POST["gender"]) && !empty(trim($_POST["gender"]))  && 
+    isset($_POST["country"]) && !empty(trim($_POST["country"]))  
+){
