7d19bae48c5a8177afd1d256399a70d15b2b911f
HalaKorhani <halakorhani2005@icloud.com>
2025-04-25 20:25:58 +0300

    "Fix duplicate input IDs and update password input type"

---
+++ Auth/register.php
@@ -0,0 +1,20 @@
+<div class="mb-3">
+    <label for="firstName" class="form-label">First Name</label>
+    <input name="fname" type="text" class="form-control" id="firstName" required>
+</div>
+<div class="mb-3">
+    <label for="lastName" class="form-label">Last Name</label>
+    <input name="lname" type="text" class="form-control" id="lastName" required>
+</div>
+<div class="mb-3">
+    <label for="address" class="form-label">Address</label>
+    <input name="address" type="text" class="form-control" id="address" required>
+</div>
+<div class="mb-3">
+    <label for="email" class="form-label">Email</label>
+    <input name="email" type="email" class="form-control" id="email" required>
+</div>
+<div class="mb-3">
+    <label for="password" class="form-label">Password</label>
+    <input name="password" type="password" class="form-control" id="password" required>
+</div>
