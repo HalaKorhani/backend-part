00ddb1c7a4da293dd37a64e7284f384e17d39859
HalaKorhani <halakorhani2005@icloud.com>
2025-04-27 00:07:52 +0300

    Select All Questions from Database

--- action/questionDelete.php
+++ action/questionDelete.php
@@ -3,4 +3,9 @@ header("Content-Type: application/json");
 session_start();
 if(isset($_POST["id"]) && !empty(trim($_POST["id"])) && is_numeric($_POST["id"])){
     $id = trim($_POST["id"]);
-   
+    require_once("../connection.php");
+    $sql = "SELECT * FROM `questions`";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $questions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  
