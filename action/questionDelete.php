2c0d99b818fc90fda57c524fe43d93c3e592c363
HalaKorhani <halakorhani2005@icloud.com>
2025-04-27 00:08:15 +0300

    Check if the ID Exists in the Questions Array

--- action/questionDelete.php
+++ action/questionDelete.php
@@ -8,4 +8,10 @@ if(isset($_POST["id"]) && !empty(trim($_POST["id"])) && is_numeric($_POST["id"])
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
     $questions = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  
+    $questionArray = [];
+    foreach($questions as $q){
+        $questionArray[] = $q["id"];
+    }
+    if(!in_array($id , $questionArray)){
+        echo json_encode(["error" => "error"]);
+    }
