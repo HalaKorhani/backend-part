44b64a16850f141d3374fbea1a588c7e6b99c738
HalaKorhani <halakorhani2005@icloud.com>
2025-04-27 00:12:04 +0300

    Retrieve User and Quiz Data

--- score/scores.php
+++ score/scores.php
@@ -2,3 +2,6 @@
 require_once("../connection.php");
 session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $quizID = $_POST['quizID'];
+    $userID = $_SESSION['userID'];
+    $score = 0;
