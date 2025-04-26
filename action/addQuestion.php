bbdfd4bd7032bd4fcf2220dc219ae8cc918f6ea2
HalaKorhani <halakorhani2005@icloud.com>
2025-04-27 00:02:31 +0300

    Convert Options to JSON

--- action/addQuestion.php
+++ action/addQuestion.php
@@ -5,3 +5,4 @@ $quizID = $_POST['quizID'];
 $questionText = $_POST['questionText'];
 $options = $_POST['options'];
 $correct_answer_index = $_POST['correct_answer_index'];
+$optionsJson = json_encode($options);
