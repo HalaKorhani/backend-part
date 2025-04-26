e22fae5611a96545dce9eb1f1648f4888e0f8520
HalaKorhani <halakorhani2005@icloud.com>
2025-04-27 00:07:25 +0300

    Check if the ID is Provided and Valid

--- action/questionDelete.php
+++ action/questionDelete.php
@@ -1,3 +1,6 @@
 <?php
 header("Content-Type: application/json");
 session_start();
+if(isset($_POST["id"]) && !empty(trim($_POST["id"])) && is_numeric($_POST["id"])){
+    $id = trim($_POST["id"]);
+   
