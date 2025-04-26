0bcdb89dc208b8309063dab5a39900f64077c2a9
HalaKorhani <halakorhani2005@icloud.com>
2025-04-27 00:01:50 +0300

    Receive Data from Form (POST)

--- action/addQuestion.php
+++ action/addQuestion.php
@@ -1,3 +1,7 @@
 <?php
 // Include the database connection file to establish the connection to the database
 require_once("../connection.php");
+$quizID = $_POST['quizID'];
+$questionText = $_POST['questionText'];
+$options = $_POST['options'];
+$correct_answer_index = $_POST['correct_answer_index'];
