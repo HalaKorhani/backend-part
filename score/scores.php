6e7268850a6230c8de9810a15cccb650539a86c4
HalaKorhani <halakorhani2005@icloud.com>
2025-04-27 00:12:32 +0300

    Iterate Over POST Data for Each Question

--- score/scores.php
+++ score/scores.php
@@ -5,3 +5,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $quizID = $_POST['quizID'];
     $userID = $_SESSION['userID'];
     $score = 0;
+    foreach ($_POST as $key => $selectedOption) {
+        if (strpos($key, 'question_') === 0) {
+            $questionID = substr($key, 9); // Remove "question_" from the ID
+
+            foreach ($_POST as $key => $selectedOption) {
+                if (strpos($key, 'question_') === 0) {
+                    $questionID = substr($key, 9); // Remove "question_" from the ID
+        
