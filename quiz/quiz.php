1c0efc6b825d3826d4993461a1e5ed686fe977d1
HalaKorhani <halakorhani2005@icloud.com>
2025-04-26 19:07:35 +0300

    add Page Heading and Table Structure

--- quiz/quiz.php
+++ quiz/quiz.php
@@ -10,3 +10,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
+<body>
+    <h2 class="display-1 text-center">quiz</h2>
+
+    <!-- Quiz Table -->
+    <table class="table table-dark table-striped">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">TITLE</th>
+                <th scope="col">Delete</th>
+            </tr>
+        </thead>
